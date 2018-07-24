cd47a8bfcc00b76bba3148f301b949195e9df4c8
pb <pijush.barik8@gmail.com>
2018-07-25 00:30:38 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ const Display = props => {
 /* Keypad */
 const Keypad = props => {
   const handlers = Object.assign({}, props.handlers);
+
+  /* Key properties. HTML buttons will be created on these properties */
   const keyProperties = [
     [
       {
@@ -51,7 +53,6 @@ const Keypad = props => {
       }
     ],
     [
-      
       {
         id: "four",
         text: "4",
@@ -72,7 +73,7 @@ const Keypad = props => {
       },
       {
         id: "multiply",
-        text: "X",
+        text: "x",
         class: "symbol",
         handler: handlers.handleOperator
       }
@@ -114,7 +115,7 @@ const Keypad = props => {
         id: "zero",
         text: "0",
         class: "digit",
-        handler: handlers.handleDigit
+        handler: handlers.handleZero
       },
       {
         id: "equals",
@@ -130,25 +131,31 @@ const Keypad = props => {
       },
     ]
   ];
+  // Array, will contain buttons
   var keys = [];
   for(let i = 0; i < keyProperties.length; i++) {
+    // Local array, will contain buttons for each row
     let row = [];
     for(let j = 0; j < keyProperties[i].length; j++) {
       let property = keyProperties[i][j];
+      // Create the button with current properties
       let button = <button key={property.id} 
                     id={property.id}
                     className={property.class}
                     onClick={property.handler}>
-                    {property.text} </button>
+                    {property.text}</button>
+      // Push the button in the row array
       row.push(button);
     }
+    // push all the buttons in the keys array and assign id in every row
     keys.push(<div key={i} id={"row-" + i}>{row}</div>);
   }
+
+  // Return the buttons inside a div having id 'keypad'
   return <div id="keypad">
     {keys}
   </div>
 };
-
 
 /* Main app component */
 class App extends React.Component {
@@ -161,27 +168,12 @@ class App extends React.Component {
 
     this.clearButtonClickHandler = this.clearButtonClickHandler.bind(this);
     this.digitsButtonClickHandler = this.digitsButtonClickHandler.bind(this);
+    this.zeroButtonClickHandler = this.zeroButtonClickHandler.bind(this);
     this.decimalButtonClickHandler = this.decimalButtonClickHandler.bind(this);
     this.operatorButtonClickHandler = this.operatorButtonClickHandler.bind(this);
     this.equalButtonClickHandler = this.equalButtonClickHandler.bind(this);
-  }
 
-  /* Clears the display area */ 
-  clearDisplay() {
-    this.setState({
-      display: ''
-    });
-  }
-  /* Returns the last number of the expression */
-  getLastValue(expression) {
-    var value = ''
-    for(let i = expression.length - 1; i >= 0; i--) {
-      if(['+', '-', 'X', '/'].some(val => val == expression[i])) {
-        break;
-      }
-      value = expression[i] + value;
-    }
-    return value;
+    var equalButtonClicked = false;
   }
 
   /* AC button handler */
@@ -194,44 +186,201 @@ class App extends React.Component {
 
   /* Numbers button handler */
   digitsButtonClickHandler(event) {
-    this.initDisplayWith('');
-    var keyValue = $(event.target)[0].childNodes[0].data;
-    var expr = this.state.display;
-    /* Replace expr with keyValue when expr is zero
-       or append keyValue to expr when expr is non-zero */
-    if(expr == '0') {
-      expr = keyValue;
-    } else {
-      expr += keyValue;
+    var btnClicked = $(event.target).text().trim(); // clicked button
+    var expression = this.state.expression; // current expression value
+    var display = this.state.display; // current display value
+
+    /* If digits are clicked after clicking equals button,
+    initialize the display and expression value first */
+    if(this.equalButtonClicked) {
+      expression = '';
+      display = '0';
+      
+      // set the equalBtnClicked to false
+      this.equalButtonClicked = false;
     }
 
+    /* If the display have any operator set it to zero first */
+    if(this.isOperator(display)) {
+      display = '0';
+    }
+
+    /* If the current display value is zero then replace this with the
+    btnClicked value. Else append the btnClicked value with the current
+    display value */
+    if(display == '0') {
+      display = btnClicked;
+    } else {
+      display += btnClicked;
+    }
+
+    /* Set the state */
     this.setState({
-      display: expr
+      display: display,
+      expression: expression + btnClicked
+    });
+  }
+
+  /* Zero button handler */
+  zeroButtonClickHandler() {
+    /* Retrieve the current display and expression values */
+    var expression = this.state.expression;
+    var display = this.state.display;
+
+    /* If the display have any operator set it to zero first */
+    if(this.isOperator(display)) {
+      display = '0';
+    }
+    
+    /* If zero button clicked after clicking equals button,
+    initialize the display and expression value first */
+    if(this.equalButtonClicked) {
+      expression = '';
+      display = '0';
+      
+      // set the equalBtnClicked to false
+      this.equalButtonClicked = false;
+    }
+
+    /* Zero will be appended with current display value
+    only if the display value is non-zero */
+    if(display != '0') {
+      display += '0';
+      expression += '0';
+    }
+
+    /* Set the state */
+    this.setState({
+      expression: expression,
+      display: display
     });
   }
 
   /* Decimal button handler */
   decimalButtonClickHandler() {
-    this.initDisplayWith('');
-    var expr = this.state.display;
-    var value = this.getLastValue(expr);
-    if(value.indexOf('.') < 0) {
-      expr += '.';
+    /* Retrieve the current display and expression values */
+    var expression = this.state.expression;
+    var display = this.state.display;
+
+    var lastOperand = this.getLastOperand(expression);
+
+    /* If the display have any operator set it to zero first */
+    if(this.isOperator(display)) {
+      display = '0';
     }
-    
+
+    /* If decimal is clicked after clicking equals button,
+    initialize the display and expression value first */
+    if(this.equalButtonClicked) {
+      expression = '';
+      display = '0';
+      
+      // set the equalBtnClicked to false
+      this.equalButtonClicked = false;
+    }
+
+    /* Append the decimal if and only 
+    if the lastOperand has no decimal points */
+    if(lastOperand.indexOf('.') < 0) {
+      display += '.';
+      expression += '.';
+    }
+
+    /* Set the state */
     this.setState({
-      display: expr
+      expression: expression,
+      display: display
     });
   }
 
   /* Operators button handler */
   operatorButtonClickHandler(event) {
-    
+    /* Retrieve the current expression and display values */
+    var display = this.state.display;
+    var expression = this.state.expression;
+
+    var operator = $(event.target).text().trim();
+
+    /* If operatore button is pressed after clicking 
+    equal button, the operation will be performed on current result
+    which is stored in display */
+    if(this.equalButtonClicked) {
+      // set the value of the expression to the current display value
+      expression = display;
+
+      // Reset equalButtonClicked
+      this.equalButtonClicked = false;
+    }
+
+    // set display to operator
+    display = operator;
+
+    /* If the last character of the expression is an operator,
+    replace that operator with current operator value, else
+    append the operator to the expression */
+    var indexOfLastChar = expression.length - 1;
+    if(this.isOperator(expression[indexOfLastChar])) {
+      // replace
+      expression = expression.slice(0, indexOfLastChar) + operator;
+    } else {
+      // append
+      expression += operator;
+    }
+
+    /* Set the state */
+    this.setState({
+      expression: expression,
+      display: display
+    });
   }
   
   /* Equal button handler */
   equalButtonClickHandler() {
+    var expression = this.state.expression; // current expression value
+
+    /* Remove any trailing operator symbols */
+    if(this.isOperator(expression[expression.length - 1])) {
+      expression = expression.slice(0, expression.length - 1);
+    }
     
+    /* In the expression multiplication symbol is represented as
+    'x'. First replace those 'x' with '*' */
+    var expressionToEvaluate = expression.replace(/x/g, '*');
+
+    /* Evaluate and set the state with current values; if 
+    and only if expression contains any value */
+    if(expression != '') this.setState({
+      expression: expression,
+      display: (+eval(expressionToEvaluate).toFixed(4)).toString()
+    });
+
+    // set the equalBtnClicked to true
+    this.equalButtonClicked = true;
+  }
+
+  /* Returns true if the argument is an operator,
+  otherwise false */
+  isOperator(char) {
+    return ['x', '/', '+', '-'].some(op => op == char);
+  }
+
+  /* Returns the last operand of expression atgument */
+  getLastOperand(expression) {
+    /* Reverse the current expression */
+    var localExpression = expression.split('').reverse().join('');
+
+    var value = '';
+    /* Check and append the character with value until found
+    an operator or reach at the end */
+    for(let i = 0; i < localExpression.length; i++) {
+      let char = localExpression[i];
+      if(this.isOperator(char)) {
+        break;
+      }
+      value += char;
+    }
+
+    return value;
   }
 
   render() {
@@ -245,6 +394,7 @@ class App extends React.Component {
         {
           handleClear: this.clearButtonClickHandler,
           handleDigit: this.digitsButtonClickHandler,
+          handleZero: this.zeroButtonClickHandler,
           handleDecimal: this.decimalButtonClickHandler,
           handleOperator: this.operatorButtonClickHandler,
           handleEqual: this.equalButtonClickHandler
